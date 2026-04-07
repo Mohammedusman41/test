7d508e5c91d296a6db4410316bc76493463b1a95
usman <usman@gmail.com>
2026-04-07 11:29:40 +0530

    1st commit from test branch

--- main.java
+++ main.java
@@ -1,6 +1,9 @@
-public class HelloWorld {
+public class LoopExample {
     public static void main(String[] args) {
-        // Prints "Hello, World!" to the terminal window.
-        System.out.println("Hello, World!");
+        // This loop prints numbers 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
     }
 }
+
